442da6c0173271783e5afd7e675811956b1cbea4
Jakub Sw <nja.mt2@gmail.com>
2020-07-26 16:33:49 +0200

    Module2 added

---
+++ module2/product.js
@@ -0,0 +1,7 @@
+module.exports = class Product {
+    constructor(id_, netPrice_) {
+        this.id = id_;
+        this.netPrice = netPrice_;
+    }
+
+};
---
+++ module2/vatService.js
@@ -0,0 +1,21 @@
+const Product = require("./product")
+module.exports = class VatService{
+    constructor() {
+    this.vatValue = 0.23;
+    }
+
+    getGrossPrice(netPrice, vatValue){
+        if(vatValue >= 1){
+            throw new Error("Vat must be lower!");
+        }else{
+            return (netPrice * (1 + vatValue));
+        }
+
+    }
+    getGrossPriceForDefaultVat(product){
+        if(product instanceof Product){
+            return (product.netPrice * (1 + this.vatValue));
+        }
+    }
+
+}
