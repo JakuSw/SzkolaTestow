36946f89cc8c9129cd2ee7ea691d0863837659c6
Jakub Sw <nja.mt2@gmail.com>
2020-08-12 22:34:00 +0200

    Module 4 added

--- module4/calculateString.js
+++ module4/calculateString.js
@@ -2,23 +2,80 @@ function calculateString(input){
     if(input === undefined){
         return 0;
     }else {
-        if(validateString(input)){
-            result = 0;
-            numbers = input.split(new RegExp(',|\n'));
-            numbers.forEach(number => {
-                if(!isNaN(number)){
+        let result = 0;
+        let separator = setSeparator(input);
+        let errorString = '';
+        let arrayOfNegativeNumberErrors = [];
+
+        input = removeCustomSeparatorDeclaration(input);
+        numbers = input.split(new RegExp( separator + '|\n'));
+
+        if(!validateString(input,separator)){
+            errorString = errorString + (String.raw`Number expected but new line found at position ` + input.search(',\n').toString());
+        }
+
+        if(numbers[numbers.length -1] == ''){
+            errorString = errorString + 'Number expected but EOF found';
+        }
+
+        numbers.forEach(number =>{
+            if(number < 0){
+                arrayOfNegativeNumberErrors.push(number);
+           }
+        });
+        
+        if(arrayOfNegativeNumberErrors.length !== 0){
+            errorString = errorString + (String.raw`Negative not allowed : ` + arrayOfNegativeNumberErrors);
+        }
+
+        if(!separatorAfterSeparator(input, separator)){
+            errorString = errorString +  `Number expected but '${separator}' found at position ${input.search(separator+separator)+1}`;
+        }
+        
+        if(errorString != ''){
+            return errorString;
+        }
+        numbers.forEach(number => {
+            if(!isNaN(number)){
+                    if(number >= 0)
+                {
                     result += parseInt(number);
                 }
-            });
-            return result;
-        }else{
-            return (String.raw`Number expected but new line found at position ` + input.search(',\n').toString());
-        }
+            }
+        });
+        return result;
     }
 };
 
-function validateString(stringToCheck){
-    stringToCheck = stringToCheck.search(',\n');
+function validateString(stringToCheck, separator){
+    stringToCheck = stringToCheck.search(separator+'\n');
+    if(stringToCheck !== -1){
+        return false;
+    }else{
+        return true;
+    }
+}
+
+function setSeparator(stringToCheck){
+    if(stringToCheck.startsWith('//')){
+        let endingIndexToSeparator = stringToCheck.search('\n');
+        return stringToCheck.slice(2,endingIndexToSeparator);
+    }else{
+        return ',';
+    }
+
+}
+
+function removeCustomSeparatorDeclaration(stringWithDeclaration){
+    if(setSeparator(stringWithDeclaration) != ','){
+        let endingIndexToSeparator = stringWithDeclaration.search('\n');
+        return stringWithDeclaration.slice(endingIndexToSeparator+1,stringWithDeclaration.length);
+    }else{
+        return stringWithDeclaration;
+    }
+}
+function separatorAfterSeparator(stringToCheck,separator){
+    stringToCheck = stringToCheck.search(separator+separator);
     if(stringToCheck !== -1){
         return false;
     }else{
--- module4/calculateString.test.js
+++ module4/calculateString.test.js
@@ -21,3 +21,18 @@ test(String.raw`Calculator should ignore new line and return 6 when 1\n2,3 given
 test(String.raw`Calculator should return error when 1,\n3 given`, () => {
     expect(calculateString('1,\n3')).toBe(String.raw`Number expected but new line found at position 1`);
 });
+test(String.raw`Calculator should return error when 1,3, given, number expected`, () => {
+    expect(calculateString('1,3,')).toBe('Number expected but EOF found');
+});
+
+test('Calculator should return 3 when //;\n1;2 given, custom separator ', () => {
+    expect(calculateString('//;\n1;2')).toBe(3);
+});
+
+test('Calculator should error and print negative numbers in string', () => {
+    expect(calculateString('-1,2')).toBe('Negative not allowed : -1');
+});
+
+test('Calculator should return multiple errors', () => {
+    expect(calculateString('-1,,2')).toBe(String.raw`Negative not allowed : -1Number expected but ',' found at position 3`);
+});
